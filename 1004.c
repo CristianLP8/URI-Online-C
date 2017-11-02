a2ffcec93a799b248c72de95339cdd20c4ea7808
Cristian Luís Pereira <cl.pereira08@gmail.com>
2017-11-02 20:49:39 -0200

    1004 - Produto Simples

---
+++ 1004.c
@@ -0,0 +1,17 @@
+#include<string.h>
+#include<math.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+int main ()
+{
+	int A, B, PROD;
+	scanf("%i", &A);
+	scanf("%i", &B);
+	PROD = A*B;
+	printf("PROD = %i\n", PROD);
+
+
+
+	return 0;
+}
