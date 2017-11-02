f7964d1a4ffd717eb49e67c161201f25d8469838
Cristian Luís Pereira <cl.pereira08@gmail.com>
2017-11-02 19:43:40 -0200

    1079 - Médias Ponderadas

---
+++ 1079.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+
+int main ()
+{
+	int n;
+    float a, b, c;
+	
+    scanf("%d", &n);
+	
+    while (n--){
+        scanf("%f %f %f", &a, &b, &c);
+        printf("%.1f\n", (a * 2.0 + b * 3.0 + c * 5.0) / 10.0);
+    }
+	
+	return 0;
+}
