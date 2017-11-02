c3c519394368f8cc207b21957dfa545a34deaadc
Cristian Luís Pereira <cl.pereira08@gmail.com>
2017-11-02 20:36:38 -0200

    1153 - Fatorial Simples

---
+++ 1153.c
@@ -0,0 +1,13 @@
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+#include<stdio.h>
+int main()
+{
+int fat, n;
+scanf("%d", &n);
+for(fat = 1; n > 1; n = n - 1)
+fat = fat * n;
+printf("%d\n", fat);
+return 0;
+}
