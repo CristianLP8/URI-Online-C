ecb6c0ef5b162c3ddc2f87e4d61706bff099ac19
Cristian Luís Pereira <cl.pereira08@gmail.com>
2017-11-02 20:49:13 -0200

    1003 - Soma Simples

---
+++ 1003.c
@@ -0,0 +1,15 @@
+#include<string.h>
+#include<math.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+int main ()
+{
+	int A, B, Soma;
+	scanf ("%i", &A);
+	scanf ("%i", &B);
+	Soma = A + B;
+	printf ("SOMA = %i\n", Soma);
+
+	return 0;
+}
