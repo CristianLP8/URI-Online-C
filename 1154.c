fe61485808f23685d79e90155d2b7024b2a8c9f8
Cristian Luís Pereira <cl.pereira08@gmail.com>
2017-11-02 19:42:43 -0200

    1154 - Idades

---
+++ 1154.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+
+int main ()
+{
+	float sum = 0.0;
+    int n, count = 0;
+
+    while (scanf("%d", &n) && n >= 0) {
+        sum += n;
+        count += 1;
+    }
+	
+    printf("%.2f\n", sum / count);
+	
+	return 0;
+}
