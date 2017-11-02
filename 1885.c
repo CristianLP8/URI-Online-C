0bb622724e69f6be8ffd275282f2072b36129649
Cristian Luís Pereira <cl.pereira08@gmail.com>
2017-11-02 19:49:14 -0200

    1185 - Acima da Diagonal Secundária

---
+++ 1885.c
@@ -0,0 +1,42 @@
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+ 
+int main ()
+{
+    int Lin, Col, Cont = 0;
+    float M[12][12], Soma = 0, Media = 0;
+    char O;
+     
+    scanf(" %c", &O);
+     
+    for(Lin = 0; Lin < 12; Lin++)
+        for(Col = 0; Col < 12; Col++)
+    {
+        scanf("%f", &M[Lin][Col]);
+    }
+     
+	if(O == 'M')
+	{
+		for(Lin = 0; Lin < 12; Lin++)
+			for(Col = 0; Col <= (10 - Lin); Col++)
+		{
+				Soma += (M[Lin][Col]);	
+		}
+		Media = Soma/66;
+		printf("%.1f\n", Media);
+	}
+	
+	if (O == 'S')
+		{
+		for(Lin = 0; Lin < 12; Lin++)
+			for(Col = 0; Col <= (10 - Lin); Col++)
+		{
+				Soma += (M[Lin][Col]);		
+		}
+		printf("%.1f\n", Soma);
+	}
+    	
+    return 0;
+}
